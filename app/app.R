f952eddeb380cc6f31fab7a700142adbb2902439
josuechinchilla <jc3635@cornell.edu>
2025-07-11 14:38:17 -0400

    created app

---
+++ app/app.R
@@ -0,0 +1,189 @@
+require(shiny)
+require(viridis)
+require(BIGr)
+require(scales)
+require(tidyverse)
+require(openxlsx)
+
+#### Helper Functions ####
+format_percent <- function(x) {
+  percent_format(accuracy = 0.1)(x)
+}
+
+# Load static reference panel
+reference <- as.data.frame(
+  read.table(
+    "https://raw.githubusercontent.com/josuechinchilla/RHBreedTools/main/data/2010_reference.txt",
+    header = TRUE,
+    row.names = 1,
+    sep = "\t"
+  ) %>%
+    dplyr::select(-c(1, 2)) %>%
+    t()
+)
+
+reference_ids <- read.table(
+  "https://raw.githubusercontent.com/josuechinchilla/RHBreedTools/main/data/ref_ids.txt",
+  header = TRUE,
+  sep = "\t"
+)
+ref_ids <- lapply(as.list(reference_ids), as.character)
+
+#### UI ####
+ui <- fluidPage(
+  tags$head(
+    tags$style(HTML("
+      .header-img {
+        text-align: center;
+        margin-bottom: 20px;
+      }
+      .header-img img {
+        max-width: 80%;
+        height: auto;
+      }
+    "))
+  ),
+  
+  div(class = "header-img",
+      img(src = "logos.png", alt = "Logos")
+  ),
+  
+  titlePanel("Russian Honeybee (RHB) content estimation"),
+  
+  fluidRow(
+    column(12,
+           wellPanel(
+             HTML('
+             <ul>
+               <li>This tool was developed by <strong>Breeding Insight</strong> in collaboration with the USDA Honey Bee Breeding, Genetics, and Physiology Research Lab.</li>
+               <li>It estimates the proportion of <strong>Russian Honey Bee (RHB)</strong> ancestry in genotype samples using methods from 
+                 <a href="https://www.animalsciencepublications.org/publications/tas/articles/1/1/36" target="_blank">Funkhouser et al. (2017)</a>.
+               </li>
+               <li><strong>Input format:</strong> Upload a genotype matrix (.txt) with SNPs in rows and samples in columns. The first three columns must be <code>ID</code>, <code>ref</code>, and <code>alt</code>. Following columns contain genotype calls (0,1,2).</li>
+               <li>Example format:
+                 <pre>
+ID    ref  alt  Sample1  Sample2  Sample3 ...
+SNP1  T    A    0        0        0
+SNP2  G    C    0        0        0
+SNP3  A    C    1        1        1
+                 </pre>
+               </li>
+               <li>Results can be downloaded as an Excel file.</li>
+             </ul>
+           ')
+           )
+    )
+  ),
+  
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("validation_file", "Upload Genotypes to test (.txt)", accept = ".txt"),
+      actionButton("run", "Run Estimation"),
+      br(), br(),
+      downloadButton("download_results", "Download Excel Results")
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Results Table", DT::dataTableOutput("preview")),
+        tabPanel("Ancestry Plot", plotOutput("bar_plot"))
+      ),
+      verbatimTextOutput("status")
+    )
+  )
+)
+
+#### Server ####
+server <- function(input, output, session) {
+  result_data <- reactiveVal(NULL)
+  result_filename <- reactiveVal(NULL)
+  
+  observeEvent(input$run, {
+    req(input$validation_file)
+    
+    output$status <- renderText("Running estimation...")
+    
+    # Read validation data
+    validation <- as.data.frame(
+      read.table(
+        input$validation_file$datapath,
+        header = TRUE,
+        row.names = 1,
+        sep = "\t"
+      ) %>%
+        dplyr::select(-c(1, 2)) %>%
+        t()
+    )
+    
+    # Allele frequency
+    freq <- BIGr:::allele_freq_poly(reference, ref_ids, ploidy = 2)
+    
+    # Breed prediction
+    prediction <- as.data.frame(BIGr:::solve_composition_poly(validation, freq, ploidy = 2)) %>%
+      select(-R2) %>%
+      rename(
+        `RHB content` = RHB,
+        `non-RHB content` = non.RHB
+      )
+    
+    columns_to_select <- c("RHB content", "non-RHB content")
+    
+    pred_results <- prediction %>%
+      as.data.frame() %>%
+      rownames_to_column(var = "ID") %>%
+      mutate(
+        across(-ID, ~format_percent(.)),
+        `Predicted line` = columns_to_select[max.col(select(., all_of(columns_to_select)), ties.method = "first")]
+      )
+    
+    result_data(pred_results)
+    
+    # Save Excel file to temp
+    date_str <- format(Sys.Date(), "%Y-%m-%d")
+    filename <- paste0("RHB_estimation_", date_str, ".xlsx")
+    temp_path <- file.path(tempdir(), filename)
+    result_filename(temp_path)
+    write.xlsx(pred_results, file = temp_path, rowNames = FALSE)
+    
+    # Table
+    output$preview <- DT::renderDataTable({
+      DT::datatable(pred_results, options = list(pageLength = 10))
+    })
+    
+    # Plot
+    pred_results_long <- prediction %>%
+      as.data.frame() %>%
+      rownames_to_column(var = "ID") %>%
+      pivot_longer(
+        cols = where(is.numeric), 
+        names_to = "category", 
+        values_to = "percent"
+      )
+    
+    output$bar_plot <- renderPlot({
+      ggplot(pred_results_long, aes(x = ID, y = percent, fill = category)) +
+        geom_bar(stat = "identity") +
+        scale_fill_viridis_d(option = "D") +
+      scale_y_continuous(labels = percent_format(accuracy = 1)) +
+        labs(
+          x = "Individual ID",
+          y = "Ancestry Proportion",
+          fill = "Line"
+        ) +
+        theme_minimal() +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 8))
+    })
+    
+    output$status <- renderText("Estimation complete. File ready for download.")
+  })
+  
+  output$download_results <- downloadHandler(
+    filename = function() {
+      basename(result_filename())
+    },
+    content = function(file) {
+      file.copy(result_filename(), file)
+    }
+  )
+}
+
+shinyApp(ui, server)
--- scripts/BreedTools_2010_paper_sets.R
+++ scripts/BreedTools_2010_paper_sets.R
@@ -1,13 +1,36 @@
-library(breedTools)
-library(scales)
-library(tidyverse)
+require("BIGr")
+require("scales")
+require("tidyverse")
 
 # read in data
-reference = read.table("https://github.com/josuechinchilla/RHBreedTools/blob/main/data/2010_reference.txt", header = T, row.names = 1, sep = "\t")
+reference <- as.data.frame(
+  read.table(
+    "https://raw.githubusercontent.com/josuechinchilla/RHBreedTools/main/data/2010_reference.txt",
+    header = TRUE,
+    row.names = 1,
+    sep = "\t"
+  ) %>%
+    dplyr::select(-c(1, 2)) %>%
+    t()
+)
 
-validation = read.table("https://github.com/josuechinchilla/RHBreedTools/blob/main/data/2010_validation.txt", header = T, row.names = 1, sep = "\t")
+validation <- as.data.frame(
+  read.table(
+    "~/Desktop/GitHub/RHBreedTools/data/2010_validation.txt",
+    header = TRUE,
+    row.names = 1,
+    sep = "\t"
+  ) %>%
+    dplyr::select(-c(1, 2)) %>%
+    t()
+)
 
-reference_ids = read.table("https://github.com/josuechinchilla/RHBreedTools/blob/main/data/ref_ids.txt", header = T, sep = "\t") 
+reference_ids <- read.table(
+    "https://raw.githubusercontent.com/josuechinchilla/RHBreedTools/main/data/ref_ids.txt",
+    header = TRUE,
+    sep = "\t"
+  ) 
+
 
 ref_ids = lapply(as.list(reference_ids),as.character)
 
@@ -18,10 +41,8 @@ format_percent <- function(x) {
 }
 
 #calculate allele frequency of reference panel
-freq2 = allele_freq_poly(reference, ref_ids)
-
-devtools#calculate breed composition of validation set
-prediction2 = as.data.frame(solve_composition_poly(validation,freq2))
+freq = BIGr:::allele_freq_poly (reference, ref_ids, ploidy = 2)
+prediction = as.data.frame(BIGr:::solve_composition_poly(validation,freq))
 
 
 # Apply the formatting function to all columns in the dataframe and create "assigned_breed" column as the breed with the highest value
@@ -29,21 +50,32 @@ prediction2 = as.data.frame(solve_composition_poly(validation,freq2))
 columns_to_select <- names(prediction)[-which(names(prediction) == "R2")]  # Exclude column R2
 
 pred_results <- prediction %>%
+  as.data.frame() %>%  # in case it's a matrix
+  rownames_to_column(var = "ID") %>%
   mutate(
-    across(everything(), ~format_percent(.)),
-    predicted_line = columns_to_select[max.col(prediction[columns_to_select], ties.method = "first")],
-    ID = row.names(prediction)
-  ) 
+    across(-ID, ~format_percent(.)),
+    predicted_line = columns_to_select[max.col(select(., all_of(columns_to_select)), ties.method = "first")]
+  )
 
-pred_results = inner_join(pred_results, raw_data, by = "ID")
-
-
-pred_results_formatted = pred_results %>%
+#create an output plot
+# Clean and reshape
+pred_results_long <- pred_results %>%
+  pivot_longer(cols = c(non.RHB, RHB), names_to = "category", values_to = "percent") %>%
   mutate(
-    line_match = ifelse(predicted_line == original_line,"match", "no_match")
-  ) %>%
-  select(c(5,6,4,7,3,1,2)) #adjust the col order of the final file, the number of columns depends on the number of reference populations.
-  
+    percent = as.numeric(str_remove(percent, "%"))
+  )
+
+# Plot
+ggplot(pred_results_long, aes(x = ID, y = percent, fill = category)) +
+  geom_bar(stat = "identity") +
+  scale_y_continuous(labels = scales::percent_format(accuracy = 1, scale = 1)) +
+  labs(
+    x = "Individual ID",
+    y = "Ancestry Proportion",
+    fill = "Ancestry Category"
+  ) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 8))
 
 #Write our breed composition results
 write.table(pred_results_formatted, "honeybee_regression_results_2010_paper_sets.txt", col.names = T, sep = "\t", quote = F, row.names = F)
